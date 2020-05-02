fd3335c3ae6f050e819d6f3e9939b68b4ce836ad
kav1982 <181893402@qq.com>
2020-05-02 10:28:59 +0800

    提交Script_03_11文件

---
+++ Assets/Scripts/Editor/Script_03_11.cs
@@ -0,0 +1,24 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.Reflection;
+
+[CustomEditor(typeof(Transform))]
+public class Script_03_11 : Editor
+{
+    private Editor m_Editor;
+    void OnEnable()
+    {
+        m_Editor = Editor.CreateEditor(target, Assembly.GetAssembly(typeof(Editor)).GetType("UnityEditor.TransformInspector", true));
+    }
+
+    public override void OnInspectorGUI()
+    {
+        if (GUILayout.Button("拓展按钮上"))
+        { }
+        GUI.enabled = false;
+        m_Editor.OnInspectorGUI();
+        GUI.enabled = true;
+        if (GUILayout.Button("拓展按钮下"))
+        { }  
+    }
+}
