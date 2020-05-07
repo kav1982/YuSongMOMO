7baa6b0a2f1bed27f0882744865959474a91c69d
kav1982 <181893402@qq.com>
2020-05-08 00:28:03 +0800

    123

---
+++ Assets/Scripts/Script_04_03.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_04_03 : MonoBehaviour
+{
+    void Awake()
+    {
+        Debug.Log("Awake 用于初始化并且永远只会执行一次");
+    }
+
+    void OnEnable()
+    {
+        Debug.Log("OnEnable 在脚本每次激活时执行一次");
+    }
+
+    void Start()
+    {
+        Debug.Log("Start 在初始化侯的下一帧执行，并且永远只会执行一次");        
+    }
+
+    void OnDisable()
+    {
+        Debug.Log("OnDisable 在脚本每次反激活后，执行一次");
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("应用程序退出时执行一次");
+    }
+
+}
---
+++ Assets/Scripts/Script_04_04.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_04_04 : MonoBehaviour
+{
+    void Satrt()
+    {
+        StartCoroutine(CreateCube());
+    }
+    IEnumerator CreateCube()
+    {
+        for(int i=0; i<100; i++)
+        {
+            GameObject.CreatePrimitive(PrimitiveType.Cube).transform.position = Vector3.one * i;
+            //等待一秒再执行下一个循环
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
