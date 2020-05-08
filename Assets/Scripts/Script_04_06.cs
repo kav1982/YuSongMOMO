e84bc3df54d2862b9fb59c8b27631b2b9fd4ef53
kav1982 <181893402@qq.com>
2020-05-08 23:52:23 +0800

    Script_04提交

---
+++ Assets/Scripts/Script_04_06.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Script_04_06 : MonoBehaviour
+{
+    public float updateInterval = 0.5f;
+    private float accum = 0;
+    private int frames = 0;
+    private float timeleft;
+    private string stringFps;
+
+    void Start()
+    {
+        timeleft = updateInterval;
+    }
+
+    void Update()
+    {
+        Application.targetFrameRate = 60;
+        //Application.targetFrameRate = 60f;
+        timeleft -= Time.deltaTime;
+        accum += Time.timeScale / Time.deltaTime;
+        ++frames;
+        if (timeleft <= 0.0)
+        {
+            float fps = accum / frames;
+            string format = System.String.Format("{0:F2} FPS", fps);
+            stringFps = format;
+            timeleft = updateInterval;
+            accum = 0.0F;
+            frames = 0;
+        }    
+    }
+
+    void OnGUI()
+    {
+        GUIStyle guiStyle = GUIStyle.none;
+        guiStyle.fontSize = 30;
+        guiStyle.normal.textColor = Color.red;
+        guiStyle.alignment = TextAnchor.UpperLeft;
+        Rect rt = new Rect(40, 0, 100, 100);
+        GUI.Label(rt, stringFps, guiStyle);
+    }
+}
