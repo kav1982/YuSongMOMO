9e92f2e9ce2401ef96e370f23c9d51741a8c7e2c
红色篝火 <48897680+kav1982@users.noreply.github.com>
2020-05-28 07:23:00 +0800

    4_12_13

---
+++ Assets/Scripts/Script_04_12.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Script_04_12 : ScriptableObject
+{
+    [SerializeField]
+    public List<PlayerInfo> m_PlayerInfo;
+
+    [System.Serializable]
+    public class PlayerInfo
+    {
+    	public int id;
+    	public string name;
+    }
+}
