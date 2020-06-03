2195a66f9cdfcfeccdf49d1ff51e3251f834468a
kav1982 <181893402@qq.com>
2020-06-04 07:18:17 +0800

    新脚本上传

---
+++ Assets/Scripts/RangeIntAttribute.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_SEITOR
+using UnityEditor;
+#endif
+public sealed class RangeIntAttribute : PropertyAttribute
+{
+    public readonly int min;
+
+    public readonly int max;
+
+    public RangeIntAttribute(int min, int max)
+    {
+        this.min = min;
+        this.max = max;
+    }
+}
